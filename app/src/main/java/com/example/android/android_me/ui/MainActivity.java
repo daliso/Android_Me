bbe3ba8e57ac60bbb41315642dbca159fabb3f8e
daliso <daliso.zuze@gmail.com>
2017-07-13 16:23:08 -0400

    Completed Exercise 6 Todos

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -26,6 +29,10 @@ import com.example.android.android_me.R;
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
+
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,38 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
 
         // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+
+        int bodyPartNumber = position/12;
+
+        int listIndex = position - 12*bodyPartNumber;
+
+        switch (bodyPartNumber){
+            case 0: headIndex = listIndex;
+                break;
+            case 1: bodyIndex = listIndex;
+                break;
+            case 2: legIndex = listIndex;
+                break;
+            default:break;
+        }
+
+        Bundle b = new Bundle();
+        b.putInt("headIndex", headIndex);
+        b.putInt("bodyIndex", bodyIndex);
+        b.putInt("legIndex", legIndex);
+
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+        intent.putExtras(b);
+
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 
